396e7aebd18c8d3abfd9e0b68fbd3fee0f13b450
DevMiner <devminer@devminer.xyz>
2024-06-05 14:59:55 +0200

    feat: ability to turn a SAM account type into a string

--- sam_account_type.go
+++ sam_account_type.go
@@ -10,11 +10,40 @@ const (
 	SamNonSecurityGroupObject SamAccountType = 0x10000001
 	SamAliasObject            SamAccountType = 0x20000000
 	SamNonSecurityAliasObject SamAccountType = 0x20000001
-	SamUserObject             SamAccountType = 0x30000000
-	SamNormalUserAccount      SamAccountType = 0x30000000
-	SamMachineAccount         SamAccountType = 0x30000001
-	SamTrustAccount           SamAccountType = 0x30000002
-	SamAppBasicGroup          SamAccountType = 0x40000000
-	SamAppQueryGroup          SamAccountType = 0x40000001
-	SamAccountTypeMax         SamAccountType = 0x7fffffff
+	// SamUserObject is also known as SAM_NORMAL_USER_ACCOUNT
+	SamUserObject     SamAccountType = 0x30000000
+	SamMachineAccount SamAccountType = 0x30000001
+	SamTrustAccount   SamAccountType = 0x30000002
+	SamAppBasicGroup  SamAccountType = 0x40000000
+	SamAppQueryGroup  SamAccountType = 0x40000001
+	SamAccountTypeMax SamAccountType = 0x7fffffff
 )
+
+func (t SamAccountType) String() string {
+	switch t {
+	case SamDomainObject:
+		return "Domain Object"
+	case SamGroupObject:
+		return "Group Object"
+	case SamNonSecurityGroupObject:
+		return "Non-Security Group Object"
+	case SamAliasObject:
+		return "Alias Object"
+	case SamNonSecurityAliasObject:
+		return "Non-Security Alias Object"
+	case SamUserObject:
+		return "User Object / Normal User Account"
+	case SamMachineAccount:
+		return "Machine Account"
+	case SamTrustAccount:
+		return "Trust Account"
+	case SamAppBasicGroup:
+		return "App Basic Group"
+	case SamAppQueryGroup:
+		return "App Query Group"
+	case SamAccountTypeMax:
+		return "Account Type Max"
+	default:
+		return "Unknown"
+	}
+}
